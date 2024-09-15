f322f1c9c511af00cc469cdfa9e59ece5cae3e03
Anushka Bhatnagar <84675020+AnushkaBhatnagar@users.noreply.github.com>
2024-09-15 14:20:30 -0400

    trial

--- players/default_player.py
+++ players/default_player.py
@@ -47,7 +47,9 @@ class Player:
         self.radius = radius
         self.memory = PlayerMemory()
         self.turn = 1
+        self.starting_position_set = False #check
 
+    
     def move(self, current_percept) -> int:
         """Function which retrieves the current state of the amoeba map and returns an amoeba movement
 
@@ -61,26 +63,109 @@ class Player:
                     RIGHT = 2
                     DOWN = 3
         """
+        if not self.starting_position_set: #setting starting position if not set
+            self.memory.pos = (current_percept.player_x, current_percept.player_y)
+            self.starting_position_set = True
 
+        """Decide on the next move based on the current percept."""
         self.memory.update_memory(current_percept.maze_state, self.turn)
+        
+        # Build the graph from the updated memory
+        currentGraph = build_graph_from_memory(self.memory)
+        
+        # Determine the goal node
+        if current_percept.is_end_visible:
+            target_node = (current_percept.end_x, current_percept.end_y)
+        else:
+            # If the end is not visible, choosing an intermediate node
+            target_node = self.choose_intermediate_target_node(current_percept)
+        
+        # Find shortest paths to the target node
+        minDistanceArray, parent = findShortestPathsToEachNode(currentGraph, self.memory.pos, self.turn)
+        
+        if target_node in minDistanceArray:
+            # Reconstruct the path to the target node
+            path = reconstruct_path(parent, self.memory.pos, target_node)
+            
+            # Get the next move from the path
+            # need to ensure invalid turns don't happen
+            if len(path) > 1:
+                next_move = self.get_move_direction(self.memory.pos, path[1])
+                self.memory.update_pos(next_move)
+                self.turn += 1
+                return next_move
+        
+        # If no valid path found or need to wait
+        return constants.WAIT
+
+    def choose_intermediate_target_node(self, current_percept): #when goal node is not visible
+
+        unexplored_nodes = self.get_unexplored_nodes(current_percept)
+        if unexplored_nodes:
+            return random.choice(unexplored_nodes)
+        else:
+            # Fallback to current position or some default strategy
+            return self.memory.pos
+
+    def get_move_direction(self, current_pos, next_pos): #current to next position
+        """        
+        Args:
+            current_pos (tuple): Current position (x, y).
+            next_pos (tuple): Next position (x, y).
+        
+        Returns:
+            int: Direction of movement (LEFT, UP, RIGHT, DOWN).
+        """
+        dx, dy = next_pos[0] - current_pos[0], next_pos[1] - current_pos[1]
+        if dx == -1 and dy == 0:
+            return constants.LEFT
+        elif dx == 1 and dy == 0:
+            return constants.RIGHT
+        elif dx == 0 and dy == -1:
+            return constants.UP
+        elif dx == 0 and dy == 1:
+            return constants.DOWN
+        else:
+            return constants.WAIT
+
+    def get_unexplored_nodes(self, current_percept): #Placeholder 
+        unexplored_nodes = []
+        return unexplored_nodes
+
+    
+    #def move(self, current_percept) -> int:
+        """Function which retrieves the current state of the amoeba map and returns an amoeba movement
+
+            Args:
+                current_percept(TimingMazeState): contains current state information
+            Returns:
+                int: This function returns the next move of the user:
+                    WAIT = -1
+                    LEFT = 0
+                    UP = 1
+                    RIGHT = 2
+                    DOWN = 3
+        """
+
+        #self.memory.update_memory(current_percept.maze_state, self.turn)
         # currentGraph = build_graph_from_memory(self.memory)
 
         # we want to build graph with PlayerMemory (self.memory)
-        currentGraph = build_graph_from_memory(self.memory)
-        minDistanceArray, parent = findShortestPathsToEachNode(currentGraph, (100, 100), turnNumber=self.turn)
+        #currentGraph = build_graph_from_memory(self.memory)
+        #minDistanceArray, parent = findShortestPathsToEachNode(currentGraph, (100, 100), turnNumber=self.turn)
 
         ## Look at minDistance Array and see where we can get too.. And decide where to go.
-        currentGraph.reconstruct_path(parent, (100, 100), ##"Node we want to go to"##)
+        #currentGraph.reconstruct_path(parent, (100, 100), ##"Node we want to go to"##)
 
 
         
         # currentGraph.visualize_graph_in_grid()
-        print("yay")
+        #print("yay")
         # if self.turn % 10 == 0 and self.turn != 0:
             
 
-        self.turn += 1
-        return constants.WAIT
+        #self.turn += 1
+        #return constants.WAIT
 
         # direction = [0, 0, 0, 0]
         # for maze_state in current_percept.maze_state:
