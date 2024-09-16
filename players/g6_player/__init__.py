96f9c4f3eba3dd09e7df214bb4aab58a9a65e91c
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-09-16 01:58:39 -0400

    explore: Select and find search targets
    ...while keeping track of boundaries and avoiding them

--- players/g6_player/__init__.py
+++ players/g6_player/__init__.py
@@ -1,6 +1,7 @@
 import numpy as np
 import logging
 import random
+import constants
 
 from players.g6_player.data import Move
 from timing_maze_state import TimingMazeState
@@ -32,6 +33,12 @@ class G6_Player:
         self.curr_pos = (0, 0)
         self.turn = 0
 
+        # a random unseen cell which the agent tries to navigate towards
+        self.search_target = (0, 0)
+
+        # the horizontal (1st idx) and vertical (2nd idx) edge indices
+        self.edges = [None, None]
+
     def move(self, current_percept: TimingMazeState) -> int:
         self.turn += 1
 
@@ -60,11 +67,31 @@ class G6_Player:
 
             self.seen[cell_row][cell_col] = True
 
+            self.__update_edges(cell)
+
+    def __update_edges(self, cell):
+        # if we haven't found both edges...
+        if self.edges[0] is None or self.edges[1] is None:
+            # ... and the cell is a boundary
+            # update the boundaries
+            if cell[3] == constants.BOUNDARY:
+                if self.edges[0] is None and cell[2] in (
+                    constants.LEFT,
+                    constants.RIGHT,
+                ):
+                    self.edges[0] = cell[0] + self.curr_pos[0]
+                elif self.edges[1] is None and cell[2] in (
+                    constants.UP,
+                    constants.DOWN,
+                ):
+                    self.edges[1] = cell[1] + self.curr_pos[1]
+
     def __convert_state(self, curr_state: TimingMazeState):
         # Update self.curr_maze with new state information
         pass
 
     def __update_curr_pos(self, movement: Move):
+        # TODO: If our move is banned by the simulator, we need to reverse our position
         match movement:
             case Move.LEFT:
                 self.curr_pos = (
@@ -88,7 +115,87 @@ class G6_Player:
                 )
 
     def __explore(self) -> Move:
-        return Move.LEFT
+        # if the current search target has been found
+        # find a new target
+        if self.seen[self.search_target[0], self.search_target[1]]:
+            self.__calculate_new_target()
+
+        return self.__find_best_move_towards_search_target()
+
+    def __find_best_move_towards_search_target(self) -> Move:
+        """
+        Given an unexplored search target at self.search_target,
+        Finds the best move towards that target
+        Considers borders and selects a nice pythagorean route instead of straight lines
+        (even though it doesn't really matter)
+        """
+        horizontal_dist = self.search_target[0] - self.curr_pos[0]
+        vertical_dist = self.search_target[1] - self.curr_pos[1]
+
+        # prioritize the axis with a greater distance
+        # for no real reason other than it looks nicer
+        # (and might be better later on)
+        if abs(horizontal_dist) >= abs(vertical_dist):
+            # search target is to the right
+            if horizontal_dist >= 0:
+                # check whether the border is between the target and current,
+                # if so, go the other direction
+                if self.edges[0] is not None and self.__border_between_target_and_curr(
+                    self.edges[0], self.search_target[0], self.curr_pos[0]
+                ):
+                    return Move.LEFT
+                return Move.RIGHT
+            else:
+                if self.edges[0] is not None and self.__border_between_target_and_curr(
+                    self.edges[0], self.search_target[0], self.curr_pos[0]
+                ):
+                    return Move.RIGHT
+                return Move.LEFT
+        else:
+            # search target is to the south
+            if vertical_dist >= 0:
+                if self.edges[1] is not None and self.__border_between_target_and_curr(
+                    self.edges[1], self.search_target[1], self.curr_pos[1]
+                ):
+                    return Move.UP
+                return Move.DOWN
+            else:
+                if self.edges[1] is not None and self.__border_between_target_and_curr(
+                    self.edges[1], self.search_target[1], self.curr_pos[1]
+                ):
+                    return Move.DOWN
+                return Move.UP
+
+    def __calculate_new_target(self):
+        """
+        Calculates a new search target for the agent to navigate towards
+        """
+        new_row = self.rng.integers(0, 100)
+        new_col = self.rng.integers(0, 100)
+        # TODO: this gets very inefficient when more cells have been explored
+        # For a certain threshold of explored cells, this should be dropped
+        # But this is not meant to be performant.
+        while self.seen[new_row][new_col]:
+            new_row = self.rng.integers(0, 100)
+            new_col = self.rng.integers(0, 100)
+
+        self.search_target = (new_row, new_col)
+        print(f"NEW TARGET: {self.search_target}")
+
+    def __border_between_target_and_curr(
+        self, border: int, target: int, curr: int
+    ) -> bool:
+        """
+        Takes a target coordinate, border coordinate and current coordinate.
+        Returns True if the border is between the target and current
+
+        Let's consider an example using the x-axis:
+        you're at [25, x] and want to get to [45, x], but the horizontal border is at 35. Then your global coordinates (in x-axis)
+        are [89, x], and the target is really at [9, x]. You must go the opposite way to approach the target, LEFT from [25, x] to [-55, x]
+        """
+
+        # check whether the border is between the target and current
+        return sorted([target, border, curr])[1] == border
 
     def __exploit(self, current_state: TimingMazeState) -> Move:
         if random.random() < 0.1:
