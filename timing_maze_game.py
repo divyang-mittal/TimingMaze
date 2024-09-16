96440e41687de9b336632e561ddb5a89e0e7ebab
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-09-15 21:23:18 -0400

    Import G6_Player to maze_game to enable `-p 6`

--- timing_maze_game.py
+++ timing_maze_game.py
@@ -9,6 +9,7 @@ from constants import *
 import constants
 from utils import *
 from players.default_player import Player as DefaultPlayer
+from players.G6_Player import G6_Player
 from collections import deque as queue
 import tkinter as tk
 
