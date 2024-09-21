bcd4b8999a2a669a03f5164e580a76e88d006224
Lsig <logis21@ru.is>
2024-09-21 17:49:28 -0400

    feat: Optimize update freq to O(1) from O(n^2)

--- players/g6_player/classes/door.py
+++ players/g6_player/classes/door.py
@@ -8,7 +8,7 @@ class Door:
         self.state: int = CLOSED
         self.turn: int = 0
         self.freq: int = 0
-        self.turns_open: list[int] = []
+        self.last_open = 0
 
     def update_turn(self, state: int, turn: int):
         """
@@ -17,16 +17,8 @@ class Door:
         self.state = state
         self.turn = turn
         if state == OPEN:
-            self.turns_open.append(turn)
-            if len(self.turns_open) > 1:
-                self.__update_freq()
-
-    def __update_freq(self):
-        """
-        Update frequency of door based on previous turns when open door was detected
-        """
-        frequencies = []
-        for i in range(len(self.turns_open) - 1):
-            for j in range(i, len(self.turns_open)):
-                frequencies.append(abs(self.turns_open[j] - self.turns_open[i]))
-        self.freq = gcd(*frequencies)
+            if self.freq != 0:
+                self.freq = gcd(self.freq, turn)
+            else:
+                self.freq = turn
+            self.last_open = turn
