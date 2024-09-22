edaa3b31db73b1ce29dcfb216eb8d1ba4b0e58ed
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-09-22 16:45:24 -0400

    Remove random and use self.rng

--- players/g6_player/__init__.py
+++ players/g6_player/__init__.py
@@ -1,6 +1,5 @@
 import numpy as np
 import logging
-import random
 
 from constants import UP, DOWN, LEFT, RIGHT, WAIT, BOUNDARY
 from players.g6_player.classes.typed_timing_maze_state import (
@@ -287,7 +286,7 @@ class G6_Player:
 
     def __panic_escape(self):
         curr_available_moves = self.__get_available_moves()
-        return random.choice(curr_available_moves)
+        return self.rng.choice(curr_available_moves)
 
     def __exploit(self, current_state: TypedTimingMazeState) -> Move:
         """
@@ -297,8 +296,11 @@ class G6_Player:
         b) when we are not getting closer to the target after a certain number of turns
         c) 10% random chance for any given turn
         """
-        if random.random() < 0.1:
-            return random.choice(list(Move))
+        assert current_state.end_x is not None
+        assert current_state.end_y is not None
+
+        if self.rng.random() < 0.1:
+            return self.rng.choice([move.value for move in Move])
 
         if 0 > current_state.end_x:
             return Move.LEFT
