6d0acb199f0136d5c505f52ba6c7e416c71369d3
eli09 <eli.g0901@gmail.com>
2024-09-23 12:36:14 -0400

    Modified num to follow heruistic, fixed timing score

--- players/g7/g7_player.py
+++ players/g7/g7_player.py
@@ -126,6 +126,7 @@ class Player:
             self.current_intermediate_target = None
         
         how_long_to_follow_intermediate_target = 150
+        how_long_to_follow_intermediate_target = max(self.memory.memory[self.memory.pos[0]][self.memory.pos[1]].visited * 2, 5)
         if self.current_intermediate_target and self.current_intermediate_target_age < how_long_to_follow_intermediate_target:
             self.current_intermediate_target_age += 1 # increment the age
         else: # update the intermediate target
@@ -179,7 +180,7 @@ class Player:
         distance_weight = 1
         time_weight = 1
         min_min_dist = np.min(min_dist_array)
-        max_min_dist = np.max(min_dist_array)
+        max_min_dist = np.max([i for i in np.reshape(min_dist_array, -1) if i < float("inf")])
         for pos in options:
             final_score = (
                 # Normalizing each factor
