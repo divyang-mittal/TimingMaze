cc512161bbb031230d2b7421b48c2c8f7f700df8
Bin Choi <binchoi@u.yale-nus.edu.sg>
2024-09-24 21:55:26 -0400

    refactor: Delete dead code and utilize cleaner design patterns (#17)
    * Refactor code and delete deadcode
    
    * Add more refactoring and comments
    
    * More refactoring
    
    * Fix error
    
    * Add refactoring and remove unneccessary code

--- players/group5/door.py
+++ players/group5/door.py
@@ -11,7 +11,6 @@ class DoorIdentifier:
     relative_coord: List[int] | None = None
 
 
-# TODO: doing this for every door would be time consuming; also make it more memory efficient (gc)
 def get_updated_frequency_candidates(candidates: Set[int], turn_num: int, door_state: int) -> Set[int]:
     if door_state == constants.BOUNDARY:
         return {0}
@@ -19,14 +18,5 @@ def get_updated_frequency_candidates(candidates: Set[int], turn_num: int, door_s
     if door_state == constants.OPEN:
         candidates.discard(0)
     
-
-    invalidated = set()
-    for c in candidates:
-        if c == 0:
-            continue
-
-        should_be_open, is_open = (turn_num % c == 0), (door_state == constants.OPEN)  # TODO: check this logic
-        if should_be_open != is_open:
-            invalidated.add(c)
-        
+    invalidated = {c for c in candidates if c != 0 and (turn_num % c == 0) != (door_state == constants.OPEN)}
     return candidates - invalidated
